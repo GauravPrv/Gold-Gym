398dd0d8cd47d1c87cbeef2485f3e65d67111114
Gaurav Arya <gauravaryawork@gmail.com>
2022-06-27 00:57:07 +0530

    Exercise Details Component Done

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "96d18e1da5mshab3db2380b8b870p1847a3jsn4edeb4f98a8d",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
